fca6fd27f5aa8d16e52ee25ea7949c897fcba7ed
AndrewMoldavchuk <andriufafa20058@gmail.com>
2026-04-05 13:13:59 +0300

    Initial commit: Add .NET app with xUnit tests and GitHub Actions CI

---
+++ CiCdApp.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using CiCdApp.Main;
+using Xunit;
+
+namespace CiCdApp.Tests;
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_ShouldReturnCorrectSum()
+    {
+        var calc = new Calculator();
+        var result = calc.Add(2, 2);
+        Assert.Equal(4, result);
+    }
+}
