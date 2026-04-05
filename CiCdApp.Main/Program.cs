6551f75d0000f51dec4b21bf7f06b056ebd35d6f
AndrewMoldavchuk <andriufafa20058@gmail.com>
2026-04-05 13:27:10 +0300

    Fix: Refactor Program.cs to use explicit Main method and correct namespace placement

--- CiCdApp.Main/Program.cs
+++ CiCdApp.Main/Program.cs
@@ -1,7 +1,17 @@
-﻿Console.WriteLine("Hello, DevOps CI/CD!");
+﻿using System;
 
 namespace CiCdApp.Main;
 
+// Класична точка входу
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, DevOps CI/CD!");
+    }
+}
+
+// Наш клас для тестування
 public class Calculator
 {
     public int Add(int a, int b) => a + b;
